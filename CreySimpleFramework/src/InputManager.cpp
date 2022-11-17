0395882a41f2cf7f6ef4d6fecf743f1fec94cc47
Csongor Zsolt Albert <a.csongor31@gmail.com>
2022-11-17 17:12:18 +0100

    removed debug log message

--- CreySimpleFramework/src/InputManager.cpp
+++ CreySimpleFramework/src/InputManager.cpp
@@ -10,11 +10,6 @@ void InputManager::registerInputTag( const sf::InputTag& iInputTag )
 
 void InputManager::setMousePosition( const glm::vec2& iPos )
 {
-	std::string slog = "mousepos(x,y):";
-	slog += " " + std::to_string((int)iPos.x);
-	slog += " " + std::to_string((int)iPos.y);
-	slog += "\n";
-	SFLOG(slog);
 	mousePos_ = iPos;
 }
 
